c500a86424d1fce0b14f0b0c74b62464e9b53933
Silverice <rlt.silverice@gmail.com>
2015-10-26 16:16:35 +0700

    store new vm's information to sqlite db

--- tiramisu_src/init_vm_for_user.py
+++ tiramisu_src/init_vm_for_user.py
@@ -1,24 +1,30 @@
 import os
+import time
 import subprocess
+import sqlite3
+
+
+conn = sqlite3.connect('tiramisu')
+c = conn.cursor()
 
 name = raw_input("# vm name?\n>> ")
 command = "rbd cp storage1/default storage1/" + name
 print(command)
 os.system(command)
 
-command = "cp default.xml " + name + ".xml"
+command = "cp config/default.xml config/" + name + ".xml"
 print(command)
 os.system(command)
 
-command = "sed -i -e 's/<name>default<\/name>/<name>" + name + "<\/name>/g' " + name + ".xml"
+command = "sed -i -e 's/<name>default<\/name>/<name>" + name + "<\/name>/g' config/" + name + ".xml"
 print(command)
 os.system(command)
 
-command = "sed -i -e 's/storage1\/default/storage1\/" + name + "/g' " + name + ".xml"
+command = "sed -i -e 's/storage1\/default/storage1\/" + name + "/g' config/" + name + ".xml"
 print(command)
 os.system(command)
 
-command = "sudo virsh define " + name + ".xml"
+command = "sudo virsh define config/" + name + ".xml"
 print(command)
 os.system(command)
 
@@ -26,14 +32,19 @@ command = "sudo virsh start " + name
 print(command)
 os.system(command)
 
-print "########## COMPLEATE ##########"
+print "########## Preparing machine ##########"
 
 while True:
         p = subprocess.Popen(['sudo', './kvm_findip', name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         ip, err = p.communicate()
         if ip != "":
+                print "########## Complete ##########"
                 print "Your IP : "
                 print(ip)
                 break
 
 print "Default username : centos7\nDefault password : root"
+
+c.execute("insert into vm (name,ip,status,pool) values (?,?,1,'storage1')", (name,ip,))
+conn.commit()
+c.close()
