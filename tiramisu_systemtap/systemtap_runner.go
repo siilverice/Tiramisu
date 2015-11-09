1c0a7b63d7f606c81cd0fa07bd7613dfab98ff72
Choestelus Suchdoge <sprintf.null@gmail.com>
2015-11-10 00:26:39 +0700

    Init gathering metrics

--- tiramisu_systemtap/systemtap_runner.go
+++ tiramisu_systemtap/systemtap_runner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -9,28 +10,64 @@ import (
 	"time"
 )
 
+type Iosize struct {
+	Name string `json:name`
+	Avg  int    `json:avg`
+}
+
+func timedSIGTERM(p *os.Process, d time.Duration) {
+	log.Println("couting down:", d)
+	_ = <-time.After(d)
+	log.Println("count finished, sending signal")
+	err := p.Signal(syscall.SIGTERM)
+	log.Println("signal sent")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 func main() {
 	fmt.Print()
-	iosizecmd := exec.Command("stap", "bitesize-nd.stp")
-	iosizecmd.Stdout = os.Stdout
-	iosizecmd.Stderr = os.Stderr
+	iosizecmd := exec.Command("stap", "bitesize-nd-json.stp")
+	iosizeStdoutPipe, err := iosizecmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// iosizecmd.Stdout = os.Stdout
 
-	err := iosizecmd.Start()
-	log.Println("pid:", iosizecmd.Process.Pid)
-	go func(p *os.Process) {
-		_ = <-time.After(1 * time.Minute)
-		err := p.Signal(syscall.SIGTERM)
-		if err != nil {
-			panic(err)
-		}
-
-	}(iosizecmd.Process)
+	err = iosizecmd.Start()
 	if err != nil {
 		panic(err)
 	}
+	go timedSIGTERM(iosizecmd.Process, 20*time.Second)
+	log.Println("pid:", iosizecmd.Process.Pid)
+
+	iosizeDecoder := json.NewDecoder(iosizeStdoutPipe)
+
+	openToken, err := iosizeDecoder.Token()
+	if err != nil {
+		log.Fatalf("error reading openToken: %v\n", err)
+	}
+	fmt.Printf("%v %T\n", openToken, openToken)
+
+	for iosizeDecoder.More() {
+		var message Iosize
+		err := iosizeDecoder.Decode(&message)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Executable: [%v], IO Size: [%v]\n", message.Name, message.Avg)
+	}
+
+	closeToken, err := iosizeDecoder.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v %T\n", closeToken, closeToken)
 
 	err = iosizecmd.Wait()
 	if err != nil {
 		panic(err)
 	}
+
 }
