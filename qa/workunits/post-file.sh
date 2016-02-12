cef93894bfe24101fe780d170268d1c6b8a73ab4
silverice <rlt.silverice@gmail.com>
2016-02-12 18:04:30 +0700

    clear ceph

--- qa/workunits/post-file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-what="$1"
-[ -z "$what" ] && what=/usr/share/base-files
-ceph-post-file -d ceph-test-workunit $what
-
-echo OK
--- qa/workunits/rbd/test_librbd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-ceph_test_librbd
-
-exit 0
